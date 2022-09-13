a66f0f64dfd6ae5a53fdb12c05c0ce9bf15cdb60
Rob Vanarsdall <rvanar@gmail.com>
2022-09-13 13:47:45 -0400

    add new movie added

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -3,8 +3,32 @@
 //? Make sure route is working
 //? Full url localhost:4000/movie/add
 const router = require("express").Router();
+const Movie = require("../models/movies.model");
 
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
+  console.log(req.body);
+  const {
+    movieTitle,
+    movieDescription,
+    movieYear,
+    isCurrentlyInTheaters,
+    rating,
+  } = req.body.movie;
+  const movie = new Movie({
+    movieTitle,
+    movieDescription,
+    movieYear,
+    isCurrentlyInTheaters,
+    rating,
+  });
+
+  try {
+    const newMovie = await movie.save();
+    res.json({ movie: newMovie });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+
   res.json({ message: "hello from /add" });
 });
 
