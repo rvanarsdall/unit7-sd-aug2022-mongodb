c77998605327b1e7b6780c378019a61707fbc587
Rob Vanarsdall <rvanar@gmail.com>
2022-09-14 10:07:30 -0400

    added seed data and update

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -4,7 +4,7 @@
 //? Full url localhost:4000/movie/add
 const router = require("express").Router();
 const Movie = require("../models/movies.model");
-
+const SeedData = require("../assets/seed.data.json");
 router.post("/add", async (req, res) => {
   console.log(req.body);
   const {
@@ -28,8 +28,80 @@ router.post("/add", async (req, res) => {
   } catch (error) {
     res.json({ message: error.message });
   }
-
-  res.json({ message: "hello from /add" });
 });
+
+// ? Build a route to get all the movie data ("/")
+//? Method: GET
+// ? Test the route
+router.get("/", async (req, res) => {
+  try {
+    const movie = await Movie.find();
+    res.json({ movie: movie });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+//? Create a route for delting a movie
+//? route endpoint will be ("/:id")
+//? localhost:4000/movie/
+//? Method: Delete
+
+router.delete("/:id", async (req, res) => {
+  console.log(req.params);
+  try {
+    const deletedMovie = await Movie.deleteOne({ _id: req.params.id });
+    res.json({
+      message:
+        deletedMovie.deletedCount > 0 ? "movie removed" : "movie not found",
+
+      deletedMovie: deletedMovie,
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+router.get("/insertAll", async (req, res) => {
+  try {
+    const insertedMovies = await Movie.insertMany(SeedData);
+    res.json({ movies: insertedMovies });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+//? Build a route for updating a record (UPDATE or PATCH)
+//? router.patch with endpoint of "/update/:id"
+//? Validate with a console.log(req.params)
+//? Full url  http://localhost:4000/movie/update/6321001b1537f4ec1450c738
+// ! You supply the record ID in the url
+//! In the body you supply what is needed to update.
+
+router.patch("/update/:id", async (req, res) => {
+  console.log(req.params);
+  try {
+    const movie = await Movie.findById(req.params.id);
+
+    if (req.body.movie.rating) {
+      movie.rating = req.body.movie.rating;
+    }
+    if (req.body.movie.movieTitle) {
+      movie.movieTitle = req.body.movie.movieTitle;
+    }
+
+    if (req.body.movie.movieYear) {
+      movie.movieYear = req.body.movie.movieYear;
+    }
+
+    movie.save();
+    res.json({ message: "movie updated", movie: movie });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+//? Create an endpoint that will get a record by it's id
+//? Endpoint should be ("/:id")
+//? Full URL is localhost:4000/movie/
 
 module.exports = router;
