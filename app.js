5ba5811c746a0a6fd555b512ffd0e37644640c8d
Rob Vanarsdall <rvanar@gmail.com>
2022-09-15 06:29:18 -0400

    added postman data

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 console.log(process.env);
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+app.use(cors());
 // ! Step 1 is make a connection to the database
 const mongoose = require("mongoose");
 
@@ -10,13 +11,13 @@ mongoose.connect(process.env.DATABASEURL);
 const db = mongoose.connection;
 
 const userController = require("./controllers/user.controller");
-const movieController = require("./controllers/movie.controller"); //<--- New Code
+const movieController = require("./controllers/movie.controller");
 
 db.once("open", () => console.log("Connected to the DB"));
 
 app.use(express.json());
 app.use("/user", userController);
-app.use("/movie", movieController); //<--- New Code
+app.use("/movie", movieController);
 app.listen(process.env.PORT, function () {
   console.log(`app is listening on port ${process.env.PORT}`);
 });
